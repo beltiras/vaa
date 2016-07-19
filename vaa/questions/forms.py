4cef5a4aa93f34ffa173bee1c2687eabff8f72e4
Árni St. Sigurðsson <arnisig@teqhire.com>
2016-07-19 21:42:27 +0000

    Undoing URL prepend

--- vaa/questions/forms.py
+++ vaa/questions/forms.py
@@ -45,7 +45,7 @@ class UserForm(forms.Form):
         self.helper.label_class = 'col-lg-3'
         self.helper.field_class = 'col-lg-9'
         self.helper.form_method = 'post'
-        self.helper.form_action = '/%s/userupdate/' % settings.INSTANCE_NAME
+        self.helper.form_action = '/userupdate/'
         self.helper.layout = Layout(
             'first_name',
             'last_name',
@@ -88,7 +88,7 @@ class AnswerForm(forms.Form):
         self.helper.label_class = 'col-lg-3'
         self.helper.field_class = 'col-lg-8'
         self.helper.form_method = 'post'
-        self.helper.form_action = '/%s/candans/' % settings.INSTANCE_NAME
+        self.helper.form_action = '/candans/'
         layout = zip([Field("q_%s" % q) for q in qs], [Field("t_%s"%q) for q in qs])
         self.helper.layout = Layout(*layout)
         self.helper.add_input(Submit('submit', 'Vista'))
@@ -122,7 +122,7 @@ class VoterForm(forms.Form):
         self.helper.label_class = 'col-lg-3'
         self.helper.field_class = 'col-lg-8'
         self.helper.form_method = 'post'
-        self.helper.form_action = '/%s/compare/' % settings.INSTANCE_NAME
+        self.helper.form_action = '/compare/'
         layout = zip([Field("q_%s" % q) for q in qs], [InlineRadios("i_%s"%q) for q in qs])
         self.helper.layout = Layout(*layout)
         self.helper.add_input(Submit('submit', u'Fá samanburð'))
