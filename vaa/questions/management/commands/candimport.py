5cc18a3567ad99284069c4e366415f9fbc9590c3
Bjarni R. Einarsson <bre@klaki.net>
2016-08-02 03:04:35 +0000

    Enhance candimport for updates, incomplete data and overlong emails

--- vaa/questions/management/commands/candimport.py
+++ vaa/questions/management/commands/candimport.py
@@ -27,12 +27,32 @@ class Command(BaseCommand):
         for row in data:
             f_name, l_name, ssn, email = row
             
-            if len(User.objects.filter(email=email)):
-                print "%s exists in User table, skipping %s" % (email, row)
+            username = email
+            if len(username) > 30:
+                username = username.split('@')[0]
+            if not f_name and not l_name:
+                f_name = username
+
+            existing_users = User.objects.filter(email=email)
+            if len(existing_users) == 1:
+                print "Note: %s exists in User table, updating details" % email
+                user = existing_users[0]
+                user.first_name = user.first_name or f_name
+                user.last_name = user.last_name or l_name
+                user.save()
+                if not Candidate.objects.filter(user=user).exists():
+                    candidate = Candidate(user=user, ssn=ssn, election=election)
+                    candidate.save()
+                print email, "updated"
+
+            elif len(existing_users) == 0:
+                user = User(username=username, first_name=f_name, last_name=l_name, email=email)
+                user.set_unusable_password()
+                user.save()
+                candidate = Candidate(user=user, ssn=ssn, election=election)
+                candidate.save()
+                print email, "created"
+
+            else:
+                print "Note: %s is ambiguous, skipping" % email
                 continue
-            user = User(username=email, first_name=f_name, last_name=l_name, email=email)
-            user.set_unusable_password()
-            user.save()
-            candidate = Candidate(user=user, ssn=ssn, claimed=False, election=election)
-            candidate.save()
-            print email, "created"
